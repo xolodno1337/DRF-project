96792fc7aad519953f6b60e335eedd5e64a124b4
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-05 12:11:10 +0300

    добавление NULLABLE

--- materials/models.py
+++ materials/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Course(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название', help_text='Введите название курса')
     image_course = models.ImageField(upload_to="materials/course", verbose_name="Превью курса",
-                                     help_text="Загрузите превью курса")
+                                     help_text="Загрузите превью курса", **NULLABLE)
     description = models.TextField(verbose_name='Описание курса', help_text='Введите описание курса')
 
     def __str__(self):
@@ -21,8 +23,9 @@ class Lesson(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название урока', help_text='Введите название урока')
     description = models.TextField(verbose_name='Описание урока', help_text='Введите описание урока')
     image_course = models.ImageField(upload_to="materials/lesson", verbose_name="Превью урока",
-                                     help_text="Загрузите превью урока")
-    video_url = models.URLField(max_length=300, verbose_name="Видео урока", help_text='Загрузите видео урока')
+                                     help_text="Загрузите превью урока", **NULLABLE)
+    video_url = models.URLField(max_length=300, verbose_name="Видео урока", help_text='Загрузите видео урока',
+                                **NULLABLE)
 
     def __str__(self):
         return self.title
