c71bb050b2bc35197461c829393ab92b2c0c8f7e
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-18 20:16:01 +0300

    добавление obj

--- users/permissions.py
+++ users/permissions.py
@@ -10,7 +10,7 @@ class IsModer(permissions.BasePermission):
 
 class IsOwner(BasePermission):
 
-    def has_object_permission(self, request, view):
+    def has_object_permission(self, request, view, obj):
         if request.user.is_staff:
             return True
         return request.user == view.get_object().owner
