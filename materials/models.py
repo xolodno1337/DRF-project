5e4b019002456e7899c11c88a2c2dc0ec4c98b0c
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-18 14:24:09 +0300

    добавление модели Subscription

--- materials/models.py
+++ materials/models.py
@@ -37,3 +37,17 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='Пользователь',
+                             related_name='subscription_user')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс',
+                               related_name='subscription_course')
+
+    def __str__(self):
+        return f'{self.user} - {self.course}'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
