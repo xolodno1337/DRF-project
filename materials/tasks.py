d24534a4ca713e473d2c1fc11967cc5b00304055
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-10 17:37:06 +0300

    Изменение кода, получаем клиентов без цикла

--- materials/tasks.py
+++ materials/tasks.py
@@ -7,12 +7,14 @@ from materials.models import Subscription
 @shared_task
 def sending_updates(course):
     """ Отправляет уведомление при обновлении курса. """
-    course_updates = Subscription.objects.filter(course=course.id)
-    for single_update in course_updates:
+    emails = Subscription.objects.filter(course=course.id).values_list('user__email', flat=True)
+    subject = 'Обновление материалов курса!'
+    message = f'Вышло обновление материалов курса - {course.name}'
+    if emails:
         send_mail(
-            subject='Обновление материалов курса!',
-            message=f'Вышло обновление материалов курса - {single_update.course.name}',
+            subject=subject,
+            message=message,
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[single_update.user.email]
+            recipient_list=emails
         )
     print('Сообщение отправлено')
